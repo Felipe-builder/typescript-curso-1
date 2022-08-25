8f8be86977aec90b6a334598b40cc427d748943c
Felipe-builder <felipesvascon@gmail.com>
2022-08-25 16:29:29 -0300

    Simplificando nosso código

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,18 @@
 import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
-  private negociacoes: Array<Negociacao> = [];
+  // private negociacoes: Array<Negociacao> = [];
+  private negociacoes: Negociacao[] = [];
 
   adiciona(negociacao: Negociacao) {
     this.negociacoes.push(negociacao);
   }
 
-  lista(): ReadonlyArray<Negociacao> {
+  // lista(): ReadonlyArray<Negociacao> {
+  //   return this.negociacoes;
+  // }
+
+  lista(): readonly Negociacao[] {
     return this.negociacoes;
   }
 }
